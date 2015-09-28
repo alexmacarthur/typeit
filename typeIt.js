8bd584d38d89ac81995538d60bc110f9e810e69e
Alex MacArthur <alex.macarthur@daveramsey.com>
2015-09-28 14:25:19 -0500

    Add irregular text entry to make it more life-like.

--- typeIt.js
+++ typeIt.js
@@ -10,30 +10,37 @@
    $.fn.typeIt = function(options){
 
     var settings = $.extend({
-       stringToType : 'This will be typed!',
-       typeSpeed: 200
+       stringToType : 'This is the default string. Please replace this string with your own.',
+       typeSpeed: 500,
+       lifeLike: true
     }, options);
 
     var theElement = this;
+    var typeSpeed = settings.typeSpeed;
+    var delayTime = settings.typeSpeed;
     var theString = settings.stringToType;
     var characterArray = theString.split('');
 
-    // set up the right CSS
+    // set up the CSS
     theElement.css('position','relative');
     $('body').append("<style type='text/css'>@keyframes blink {0% {opacity: 1;} 50% {opacity: 0;} 100% {opacity: 1;}} .ti-cursor{-webkit-animation: blink 1s infinite; animation: blink 1s infinite; position:absolute; right:-5px;}</style>");
 
     // add cursor
     theElement.append("<span class='ti-cursor'>|</span>");
 
+    // output the string
     var i = 0;
     function typeLoop () {
+      if(settings.lifeLike === true){
+        delayTime = typeSpeed*Math.random();
+      }
       setTimeout(function () {
         theElement.append(characterArray[i]);
         i++;
         if (i < characterArray.length) {
           typeLoop();
         }
-      }, settings.typeSpeed)
+      }, delayTime)
     }
     typeLoop();
 
