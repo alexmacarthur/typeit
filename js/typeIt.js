8612679127530f206896d608f4facdf7dba9176d
alexmacarthur <alex@macarthur.me>
2015-09-29 23:19:01 -0500

    More progress with merged arrays.

--- js/typeIt.js
+++ js/typeIt.js
@@ -9,6 +9,7 @@
 
    $.fn.typeIt = function(options){
 
+    // user-defined attribute settings
     var dataSettings = {
       whatToType : this.data('typeitString'),
       typeSpeed: this.data('typeitSpeed'),
@@ -16,6 +17,7 @@
       showCursor: this.data('typeitShowcursor')
     };
 
+    // default settings; merge with data attribute settings & function settings
     var settings = $.extend({
        whatToType : ['aaaaa','xxxxx','yyyyy'],
        typeSpeed: 500,
@@ -24,36 +26,40 @@
     }, options, dataSettings);
 
     var theElement = this;
-    var typeSpeed = settings.typeSpeed;
-    var delayTime = settings.typeSpeed;
-    var mergedStrings = settings.whatToType.join('');
 
     if(settings.showCursor === true){
       theElement.addClass('ti-cursor');
       theElement.css('position','relative');
     }
 
-    var stringArrayLength = settings.whatToType.length;
-    for (var i = 0; i < stringArrayLength; i++){
+    // call function to output text
+    typeStuff(settings.whatToType.join(''), settings.lifeLike, settings.typeSpeed, settings.whatToType);
 
-      var theString = settings.whatToType[i];
-      var characterArray = theString.split('');
+    // the function that types out text
+    function typeStuff(string, lifelike, speed, stringArray){
 
-      typeStuff();
-
-    }
-
-    function typeStuff(){
-      // output the string
       var i = 0;
+      var delayTime = settings.typeSpeed;
+
+      if(stringArray.length > 1){
+        var lengths = {};
+        for(j=0; j < stringArray.length; j++){
+          lengths[j] = stringArray[j].length;
+        }
+      }
+
       function typeLoop () {
-        if(settings.lifeLike === true){
-          delayTime = typeSpeed*Math.random();
+
+        if(lifelike === true){
+          delayTime = speed*Math.random();
         }
         setTimeout(function () {
-          theElement.append(characterArray[i]);
+
+          theElement.append(string[i]);
+
           i++;
-          if (i < characterArray.length) {
+
+          if (i < string.length) {
             typeLoop();
           }
         }, delayTime);
