7bdec4d0ccbc27277e164f724b1a352d167d337d
Alex MacArthur <alex@macarthur.me>
2015-10-23 20:55:24 -0500

    Code cleanup, add demo page.

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+// load our plugins
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var autoprefix = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+
+// check JavaScript
+gulp.task('jshint',function(){
+  gulp.src('src/typeit.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+});
+
+// minify JavaScript and put it into /dist
+gulp.task('scripts', function() {
+  gulp.src('src/typeit.js')
+    .pipe(uglify())
+    .pipe(rename('typeit.min.js'))
+    .pipe(gulp.dest('dist'));
+
+  // put the unminified JavaScript into /dist
+  gulp.src('src/typeit.js')
+    .pipe(gulp.dest('dist'));
+});
+
+// compile and autoprefix SASS
+gulp.task('typeItSass', function () {
+
+  // compile & autoprefix the SCSS in the /src folder
+  gulp.src('src/typeit.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(gulp.dest('src'));
+
+  // put minified CSS into /dist
+  gulp.src('src/typeit.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(rename('typeit.min.css'))
+    .pipe(gulp.dest('dist'));
+
+  // put unminified CSS into /dist
+  gulp.src('src/typeit.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(gulp.dest('dist'));
+
+  // put original SCSS into /dist
+  gulp.src('src/typeit.scss')
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('demoSass',function(){
+  // compile our local demo SCSS
+  gulp.src('src/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(gulp.dest('src'));
+});
+
+// run our default gulp tasks and watch for changes
+gulp.task('default', ['jshint','scripts','typeItSass','demoSass'], function() {
+
+  // watch for JavaScript changes
+  gulp.watch('src/typeit.js', ['jshint', 'scripts']);
+
+  // watch for SASS changes
+  gulp.watch('src/typeit.scss', ['typeItSass']);
+
+  // watch for SASS changes
+  gulp.watch('src/**/*.scss', ['demoSass']);
+
+});
