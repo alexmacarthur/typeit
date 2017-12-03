8cf6fffdae339b032152a841349b3a3635dbb8f0
Thiago Santos <thia.mdossantos@gmail.com>
2017-12-03 00:10:17 -0200

    Swap out Webpack for Rollup. (#66)
    * :sparkles: use rollup
    * :fire: remove webpack config file
    * :fire: remove babelrc

---
+++ rollup.config.js
@@ -0,0 +1,70 @@
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import pkg from './package.json'
+
+const banner = `/*!
+ *
+ *   ${pkg.name} - ${pkg.description}
+ *   Author: ${pkg.author}
+ *   Version: v${pkg.version}
+ *   URL: ${pkg.homepage}
+ *   License: ${pkg.license}
+ *
+ */`
+
+const plugins = [
+  babel({
+    babelrc: false,
+    presets: [
+      [
+        'env',
+        {
+          modules: false
+        }
+      ]
+    ],
+    plugins: ['external-helpers']
+  })
+]
+
+export default [
+  {
+    input: 'src/typeit.js',
+    plugins: plugins,
+    output: [
+      {
+        file: 'dist/typeit.js',
+        format: 'umd',
+        name: 'TypeIt',
+        banner: banner
+      },
+      {
+        file: 'dist/typeit.es.js',
+        format: 'es',
+        banner: banner
+      }
+    ]
+  },
+  {
+    input: 'src/typeit.js',
+    plugins: plugins.concat([
+      uglify({
+        output: {
+          comments: function(node, comment) {
+            const text = comment.value
+            const type = comment.type
+            if (type === 'comment2') {
+              return /typeit/i.test(text)
+            }
+          }
+        }
+      })
+    ]),
+    output: {
+      file: 'dist/typeit.min.js',
+      format: 'umd',
+      name: 'TypeIt',
+      banner: banner
+    }
+  }
+]
