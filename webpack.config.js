027820dc71380a440b692b146fdcdb5926e3612d
Alex MacArthur <alex.macarthur@daveramsey.com>
2019-10-16 15:30:08 -0500

    Generate both default and modern bundles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const pkg = require("./package.json");
+const merge = require("webpack-merge");
 
 const banner = `
   ${pkg.name} - ${pkg.description}
@@ -9,15 +10,41 @@ const banner = `
   License: ${pkg.license}
 `;
 
-const plugins = [];
+const browsers = {
+  default: ["> 2%", "Last 2 versions", "safari >=9", "not ie < 11"],
+  modern: [
+    "Edge >= 16",
+    "Firefox >= 60",
+    "Chrome >= 61",
+    "Safari >= 11",
+    "Opera >= 48"
+  ]
+};
 
-plugins.push(
-  new webpack.BannerPlugin({
-    banner
-  })
-);
+const getJSRule = type => {
+  return {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        cacheDirectory: true,
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                browsers: browsers[type]
+              }
+            }
+          ]
+        ]
+      }
+    }
+  };
+};
 
-module.exports = {
+const baseConfiguration = {
   mode: process.env.NODE_ENV,
   entry: {
     TypeIt: "./src/TypeIt.js"
@@ -31,16 +58,28 @@ module.exports = {
     globalObject: "this"
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          compact: true
-        }
-      }
-    ]
+    rules: [getJSRule("default")]
   },
-  plugins
+  plugins: [
+    new webpack.BannerPlugin({
+      banner
+    })
+  ]
 };
+
+const modernConfiguration = merge.smart(baseConfiguration, {
+  output: {
+    filename: "typeit.modern.min.js"
+  },
+  module: {
+    rules: [getJSRule("modern")]
+  }
+});
+
+const output = [baseConfiguration];
+
+if (process.env.NODE_ENV === "production") {
+  output.push(modernConfiguration);
+}
+
+module.exports = output;
