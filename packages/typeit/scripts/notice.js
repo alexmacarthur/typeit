13b550bcb18af6fdbf6b5ba6e1d99e9c2db99d6d
Alex MacArthur <alex@macarthur.me>
2021-12-03 21:06:28 -0600

    Fix postinstall script.

---
+++ packages/typeit/scripts/notice.js
@@ -0,0 +1,5 @@
+const message = `Thanks for using TypeIt! If you're using this library commercially, please purchase a license:
+
+https://typeitjs.com/licenses/purchase`;
+
+console.log('\x1b[35m%s\x1b[0m', message);
