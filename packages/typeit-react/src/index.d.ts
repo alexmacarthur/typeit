a4aa2e1c66fa891a23c8d40a1c90b28b52d1c549
Santos Pierre <santospierre1305@gmail.com>
2021-12-15 20:26:39 +0100

    Typeitreact - Add type to TypeItOptions

--- packages/typeit-react/src/index.d.ts
+++ packages/typeit-react/src/index.d.ts
@@ -1,6 +1,20 @@
 import * as React from 'react';
 export interface TypeItOptions {
     strings?: Array<string> | string;
+    speed?: number;
+    deleteSpeed?: number | null;
+    lifeLike?: boolean;
+    cursor?: boolean;
+    cursorSpeed?: number;
+    cursorChar?: string;
+    breakLines?: boolean;
+    nextStringDelay?: Array<number> | number;
+    waitUntilVisible?: boolean;
+    startDelete?: boolean;
+    startDelay?: boolean;
+    loop?: boolean;
+    loopDelay?: Array<number> | number;
+    html?: boolean;
 }
 export interface TypeItProps {
     as?: keyof JSX.IntrinsicElements;
