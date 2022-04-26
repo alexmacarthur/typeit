54a87c874d7fc9c988effea367b07274b7d91be5
Alex MacArthur <alex@macarthur.me>
2022-04-25 23:35:04 -0500

    Update typeit-react.

--- packages/typeit-react/examples/index.jsx
+++ packages/typeit-react/examples/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import App from './app'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('app'));
+
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('app')
+    </React.StrictMode>
 )
