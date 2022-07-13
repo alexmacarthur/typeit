eaefade011a8f95c6fa3d6570d3fa5f29aa559dc
Alex MacArthur <alex@macarthur.me>
2022-07-12 23:50:34 -0500

    Fix bug causing root element to be deleted during rapid resets.

--- packages/typeit/__tests__/helpers/removeNode.test.js
+++ packages/typeit/__tests__/helpers/removeNode.test.js
@@ -8,18 +8,29 @@ it("removes a node", () => {
   expect(document.body.innerHTML).toEqual("<div>hi.</div>");
 });
 
-it("removes parents if they will be empty", () => {
-  setHTML`<div>hi.<h1><span id="removeMe">bye.</span></h1></div>`;
+describe("parent removal", () => {
+  it("removes parents if they will be empty", () => {
+    setHTML`<div>hi.<h1><span id="removeMe">bye.</span></h1></div>`;
 
-  removeNode(document.getElementById("removeMe"));
+    removeNode(document.getElementById("removeMe"));
 
-  expect(document.body.innerHTML).toEqual("<div>hi.</div>");
-});
+    expect(document.body.innerHTML).toEqual("<div>hi.</div>");
+  });
 
-it("removes parents if they will be empty", () => {
-  setHTML`<span>abc<i>hi!</i></span>`;
+  it("removes different parent if it will be empty", () => {
+    setHTML`<span>abc<i>hi!</i></span>`;
 
-  removeNode(document.body.querySelector("span").firstChild);
+    removeNode(document.body.querySelector("span").firstChild);
 
-  expect(document.body.innerHTML).toEqual("<span><i>hi!</i></span>");
+    expect(document.body.innerHTML).toEqual("<span><i>hi!</i></span>");
+  });
+
+  it("does not remove root element", () => {
+    setHTML`<h3 id="root"><span><i>hi!</i></span></h3>`;
+
+    const root = document.getElementById("root");
+    removeNode(document.body.querySelector("span").firstChild, root);
+
+    expect(document.body.innerHTML).toEqual('<h3 id="root"></h3>');
+  });
 });
